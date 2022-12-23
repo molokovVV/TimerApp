df0de987f04f99ea367ff8bd33a4a96b8dd3914a
Vitaliy <vmolokov59@gmail.com>
2022-12-23 21:11:36 +0300

    Prepared interface

---
+++ TimerApp/Sources/App/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  TimerApp
+//
+//  Created by Виталик Молоков on 23.12.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, 
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
